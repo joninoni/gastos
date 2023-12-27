cefb810d515e433828d5831ba65b8967afaef585
joninoni <jonibeltranhayer@gmail.com>
2023-12-27 09:53:47 -0600

    codigo para mostrar un mensaje ya sea de error o de exito

--- js/gasto.js
+++ js/gasto.js
@@ -1,5 +1,8 @@
 const formulario = document.getElementById('agregar-gasto');
 const gastosListado = document.querySelector('#gastos ul');
+const primario =document.querySelector(".primario");
+const regex = /^\d+(\.)?\d*$/;
+
 
 // Eventos
 eventListeners();
@@ -10,19 +13,39 @@ function eventListeners() {
 //Classes
 class Presupuesto {
     constructor(presupuesto){
-        this.presupuesto = presupuesto,
-        this.restante = presupuesto,
+        this.presupuesto = Number(presupuesto),
+        this.restante = Number(presupuesto),
         this.gastos =[]
     }
-    
 }
 
 class UI{
     mostrarPresupuesto(cantidad){
         const {presupuesto,restante}=cantidad;
-
         document.querySelector("#total").textContent=presupuesto;
         document.querySelector("#restante").textContent=restante;
+    }
+    mostrarMensaje(mensaje,tipo){
+        const alerta=primario.querySelector(".alert");
+        if(alerta === null){
+            const divError=document.createElement("div");
+            divError.classList.add("text-center","alert");        
+            if(tipo ==="error"){
+                divError.classList.add("alert-danger");
+            }
+            else{
+                divError.classList.add("alert-success");
+            }
+            divError.textContent=mensaje;
+            //insertando el mensaje
+            
+            primario.insertBefore(divError,formulario);
+    
+            setTimeout(() => {
+                //eliminando al mensaje
+                divError.remove();
+            }, 3000);
+        }  
     }
 }
 
@@ -31,15 +54,29 @@ const ui = new UI;
 
 //Funciones
 function preguntarPresupuesto(){
-    const presupuestoUsuario =  Number(prompt('¿Cual es tu presupuesto?'));
-    
-    if(presupuestoUsuario === ''||presupuestoUsuario === null||isNaN(presupuestoUsuario)||presupuestoUsuario<=0){
-        window.location.reload();
-    }
+    // const presupuestoUsuario =  Number(prompt('¿Cual es tu presupuesto?'));
+    const presupuestoUsuario = 50;
+    // if(presupuestoUsuario === ''||presupuestoUsuario === null||isNaN(presupuestoUsuario)||presupuestoUsuario<=0){
+    //     window.location.reload();
+    // }
     presupuesto = new Presupuesto(presupuestoUsuario);
     ui.mostrarPresupuesto(presupuesto);
 }
 function validandoFormulario(e){
     e.preventDefault();
-    console.log("hola");
+    const nombre = document.querySelector("#gasto").value;
+    const cantidad = document.querySelector("#cantidad").value;
+    if (nombre ===""  || cantidad ==="") {
+            ui.mostrarMensaje("No puede haber campos vacios","error");
+            return;
+    }
+    if(cantidad <=0 || isNaN(cantidad)){
+            ui.mostrarMensaje("Cantidad no valida","error");
+            return
+    }
+    if(regex.test(nombre)){
+        ui.mostrarMensaje("gasto no valido","error")
+        return;
+    }
+    
 }
